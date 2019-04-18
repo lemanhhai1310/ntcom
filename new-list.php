b3a6d4a21b166f6e72ce1b4b4e1a0ed34812ac75
macbook <hai.lemanh.1310@gmail.com>
2019-04-19 00:19:59 +0700

    update breadcrumb

--- new-list.php
+++ new-list.php
@@ -1,3 +1,10 @@
 <?php include('header.php'); ?>
-
+<div class="block_breadcrumb">
+    <div class="uk-container">
+        <ul class="uk-breadcrumb">
+            <li><a href="#">Trang chủ</a></li>
+            <li><span>Tin tức</span></li>
+        </ul>
+    </div>
+</div>
 <?php include('footer.php'); ?>
