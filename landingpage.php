9527a1c0b190e313d381bf2bf6d8aacaa0109bd0
macbook <hai.lemanh.1310@gmail.com>
2019-04-23 03:13:16 +0700

    update landingpage

--- landingpage.php
+++ landingpage.php
@@ -5,8 +5,12 @@
     <div class="uk-container">
         <div class="uk-flex-bottom uk-grid-collapse" uk-grid>
             <div class="uk-width-expand@m">
-                <div class="box1_ld">
-                    <h2>Điều khiển toà nhà thông minh</h2>
+                <div class="box1_ld uk-margin-medium">
+                    <h2>Điều khiển toà nhà <br> thông minh</h2>
+                    <p>Tiêu chuẩn mới của một ngôi nhà hoàn hảo <br>
+                        Đẳng cấp – Tiện nghi – an toàn
+                    </p>
+                    <a href="#" class="uk-button btn3 uk-button-secondary">đăng ký dùng thử <span uk-icon="arrow-right"></span></a>
                 </div>
                 <div class="uk-grid-collapse uk-flex-bottom" uk-grid>
                     <div class="uk-width-2-5@m">
@@ -21,6 +25,145 @@
                 <img src="imgs/landingpage/bg1-3.png" alt="">
             </div>
         </div>
+    </div>
+</div>
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-flex-middle" uk-grid>
+            <div class="uk-width-1-2@m">
+                <img src="imgs/landingpage/bg2.png" alt="">
+            </div>
+            <div class="uk-width-expand@m">
+                <div class="box2_ld">
+                    <h2>
+                        <div>BẠN ĐANG TÌM</div>
+                        GIẢI PHÁP TOÀ NHÀ THÔNG MINH?
+                    </h2>
+                    <p class="uk-margin-large-bottom">Hãy để NTCOM biến căn nhà bạn trở nên thông minh, mang lại không gian sống đẳng cấp, tiện nghi và an toàn</p>
+                    <hr class="uk-divider-small uk-margin-medium">
+                    <ul class="uk-list list1_ld">
+                        <li>
+                            <span uk-icon="check"></span>
+                            Có nghe qua về nhà thông minh nhưng chưa hiểu rõ cách hoạt động và các tiện ích đem lại
+                        </li>
+                        <li>
+                            <span uk-icon="check"></span>
+                            Lo ngại công nghệ quá mới lại khó sử dụng, đem lại phiền phức
+                        </li>
+                        <li>
+                            <span uk-icon="check"></span>
+                            Không đáp ứng được mức giá quá cao
+                        </li>
+                        <li>
+                            <span uk-icon="check"></span>
+                            Nhà sửa lại nên ngại thay đổi hệ thống điện cũ sẵn có
+                        </li>
+                    </ul>
+                    <a href="#" class="uk-button btn3 uk-button-secondary">đăng ký dùng thử <span uk-icon="arrow-right"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section block1">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <div>
+                <div class="box3_ld">
+                    <h2>Điều khiển và kiểm soát toà nhà dù bạn ở bất kỳ đâu.</h2>
+                    <p>Điều khiển đèn, điều hòa, rèm cửa... qua điện thoại hoặc máy tính bảng với phần mềm NTCOM (app iOS & Android). Ngoài ra, NTCOM tương thích kết nối được với hầu hết các loại thiết bị an ninh cảnh báo như cảm biến (báo cháy, chuyển động), camera an ninh chống trộm hiện có trên thị trường. </p>
+                </div>
+            </div>
+            <div>
+                <img src="imgs/landingpage/bg3.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section block2">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <div>
+                <div class="box3_ld">
+                    <h2>Dễ dàng sử dụng cho mọi thành viên trong gia đình.</h2>
+                    <p>Công tắc cảm ứng với hình icon biểu tượng chú thích theo tên thiết bị, đảm bảo từ người già đến trẻ em đều dễ dàng  sử dụng. Chủ nhà có thể tự cài đặt nhiều kịch bản hoạt động cho từng phím điều khiển tùy theo ý muốn và thói quen của mình như đón khách, ra khỏi nhà, về nhà, đi ngủ.... Hệ thống NTCOM tối ưu tối đa dựa trên thói quen sinh hoạt của gia đình người Việt.</p>
+                </div>
+            </div>
+            <div class="uk-flex-first@m">
+                <img src="imgs/landingpage/bg4.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section block1">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <div>
+                <div class="box3_ld">
+                    <h2>Thiết kế chuẩn Châu Âu sang trọng và đẳng cấp</h2>
+                    <p>Sử dụng ngôn ngữ thiết kế châu Âu hiện đại, mặt kính cường lực cùng phím khắc laze tinh xảo, công tắc cảm ứng NTCOM được nhiều kiến trúc sư đánh giá rất phù hợp với phong cách thiết kế nội thất phổ biến hiện nay. Biệt thự cổ điển, chung cư hiện đại hay nhà phố tân cổ điển châu Âu đều được khách hàng đánh giá cao về tính thẩm mỹ, gia tăng đẳng cấp cho nội thất ngôi nhà. </p>
+                </div>
+            </div>
+            <div>
+                <img src="imgs/landingpage/bg5.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section block2">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <div>
+                <div class="box3_ld">
+                    <h2>Đảm bảo an toàn điện tuyệt đối</h2>
+                    <p>Thiết bị của NTCOM lắp đặt trên hệ thống điện dân dụng bình thường sẵn có theo thiết kế của chủ nhà. Sửa chữa và thay thế dễ dàng mà không cần đục phá tường hay thay đổi hệ thống dây điện đi âm trong tường. </p>
+                </div>
+            </div>
+            <div class="uk-flex-first@m">
+                <img src="imgs/landingpage/bg6.png" alt="">
+            </div>
+            <div class="uk-width-1-1 uk-text-center">
+                <a href="#" class="uk-button btn3 uk-button-secondary">đăng ký dùng thử <span uk-icon="arrow-right"></span></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-child-width-1-3@m" uk-grid>
+            <div>
+                <div class="uk-text-center box11">
+                    <div class="b_img">
+                        <img src="imgs/landingpage/b1.png" alt="">
+                    </div>
+                    <p>Thiết bị NTCOM đạt chứng chỉ đảm bảo chất lương CE. Đủ điều kiện xuất khẩu châu Âu.</p>
+                    <a href="#"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-text-center box11">
+                    <div class="b_img">
+                        <img src="imgs/landingpage/b2.png" alt="">
+                    </div>
+                    <p>Thiết bị NTCOM được bảo hành 1 đổi 1 từ nhà sản xuất trong 2 năm. </p>
+                    <a href="#"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+            <div>
+                <div class="uk-text-center box11">
+                    <div class="b_img">
+                        <img src="imgs/landingpage/b3.png" alt="">
+                    </div>
+                    <p>Hỗ trợ kỹ thuật miễn phí 24/7. Kỹ thuật viên có thể hỗ trợ cài đặt từ xa.</p>
+                    <a href="#"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section block3">
+    <div class="uk-container">
+
     </div>
 </div>
 <?php include('footer.php'); ?>
