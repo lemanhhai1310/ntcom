32af8998c50580a343d576ffe29d2bee2dee4b5a
macbook <hai.lemanh.1310@gmail.com>
2019-04-22 23:48:08 +0700

    update landingpage

--- landingpage.php
+++ landingpage.php
@@ -1,4 +1,26 @@
-<?php $body = 'home' ?>
+<?php $body = '' ?>
 <?php $landingpage = ''; ?>
 <?php include('header.php'); ?>
+<div class="uk-section uk-background-cover" style="background-image: url(imgs/landingpage/bg1.jpg)">
+    <div class="uk-container">
+        <div class="uk-flex-bottom uk-grid-collapse" uk-grid>
+            <div class="uk-width-expand@m">
+                <div class="box1_ld">
+                    <h2>Điều khiển toà nhà thông minh</h2>
+                </div>
+                <div class="uk-grid-collapse uk-flex-bottom" uk-grid>
+                    <div class="uk-width-2-5@m">
+                        <img src="imgs/landingpage/bg1-2.png" alt="">
+                    </div>
+                    <div class="uk-width-expand@m">
+                        <img src="imgs/landingpage/bg1-1.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="uk-width-1-3@m">
+                <img src="imgs/landingpage/bg1-3.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
 <?php include('footer.php'); ?>
