7e7f97ed872d62daca64e3345455242ab410a3da
macbook <hai.lemanh.1310@gmail.com>
2019-04-22 16:01:40 +0700

    update trang chi tiết sản phẩm

--- product-detail.php
+++ product-detail.php
@@ -9,13 +9,122 @@
         </ul>
     </div>
 </div>
+<div class="uk-section-small" style="background-color: #fff;">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m" uk-grid>
+            <div>
+                <?php include('slide-detail.php'); ?>
+            </div>
+            <div>
+                <h1 class="title1 uk-margin-small">CISCO1941W-N / K9</h1>
+                <div class="rate1 uk-margin-small">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <span class="uk-padding-small uk-padding-remove-top uk-padding-remove-bottom">5/5.0</span>
+                    <a href="#">2 đánh giá</a>
+                </div>
+                <div class="uk-overflow-auto uk-margin">
+                    <table class="uk-table table1">
+                        <tbody>
+                        <tr>
+                            <td><b>Mô hình</b></td>
+                            <td>CISCO194W-N / K9 Bộ định tuyến không dây Cisco</td>
+                        </tr>
+                        <tr>
+                            <td><b>Chi tiết</b></td>
+                            <td>Bộ định tuyến Cisco 1941w / 802.11a / b / g / n Tốt</td>
+                        </tr>
+                        <tr>
+                            <td><b>Điều kiện</b></td>
+                            <td>
+                                <label class="select_size">
+                                    <input class="uk-radio uk-hidden" type="radio" name="radio1" checked="">
+                                    <span>Hoàn toàn mới</span>
+                                </label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><b>Tình trạng</b></td>
+                            <td>Còn hàng</td>
+                        </tr>
+                        <tr>
+                            <td><b>Số lượng</b></td>
+                            <td>
+                                <input class="uk-input uk-form-width-xsmall" type="text" placeholder="1">
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <a href="#" class="uk-button uk-button-danger btn1 uk-width-1-1">Mua ngay</a>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="uk-section-small">
     <div class="uk-container">
         <div class="uk-padding-small box1_sp" style="background-color: #FFFFFF;">
             <h4 class="title_tskt uk-text-uppercase uk-margin">mô tả sản phẩm</h4>
+            <article>
+                <p>Với thiết kế đơn giản nhưng lại rất tinh tế, cùng với độ phủ sóng rộng mà cài đặt lại dễ dàng, Thiết bị mạng Linksys WHW0303 là một sự lựa chọn tốt cho các hộ gia đình, căn biệt thự và thậm chí cả những văn phòng với không gian lớn nhờ những công nghệ hỗ trợ tiên tiến.</p>
+                <p>Sản phẩm gồm 3 thiết bị giống nhau, được cấu tạo giống kiểu loa bluetooth với kết cấu hình hộp trắng, đế cao su và đi kèm những lỗ thông hơi trên bề mặt. Với thiết kế bên ngoài như vậy, sản phẩm không chỉ đảm bảo được độ bền mà còn giúp tản nhiệt hiệu quả hơn để sản phẩm không quá nóng khi phải hoạt động liên tục trong khoảng thời gian dài.</p>
+                <figure class="uk-text-center">
+                    <img src="https://tmp.phongvu.vn/wp-content/uploads/2018/10/Thi%E1%BA%BFt-b%E1%BB%8B-m%E1%BA%A1ng-Linksys-WHW0303.jpg"
+                         alt="">
+                </figure>
+                <p>Tiếp đến, bên trong mỗi thiết bị mạng có một bộ định tuyến 3 băng tần với CPU lõi tứ Qualcomm IPQ4019 xung nhịp 716 MHz, Ram 512MB cùng bộ nhớ Flash lên đến 4GB. Bên cạnh đó, sản phẩm còn được tích hợp 6 ăng ten rời công suất cao, tạo điều kiện phủ sóng với diện tích lên tới 185m2, góp phần tạo nên 1 sản phẩm với hiệu suất ổn định và đặc biệt phù hợp với những căn hộ cao cấp hay biệt thự.</p>
+                <p>Ngoài ra, với phần mềm Linksys App được sử dụng hoàn toàn dễ dàng trên điện thoại thông minh, quá trình thiết lập hệ thống quản lý sản phẩm trở nên đơn giản hơn. Bạn cũng có thể theo dõi hoạt động, hiệu suất cũng như cập nhật Firmware cho thiết bị để nó luôn đạt trạng thái tốt nhất mỗi lần sử dụng.</p>
+                <figure class="uk-text-center">
+                    <img src="https://tmp.phongvu.vn/wp-content/uploads/2018/10/Thi%E1%BA%BFt-b%E1%BB%8B-m%E1%BA%A1ng-Linksys-WHW0303-1.jpg"
+                         alt="">
+                </figure>
+                <p>Cuối cùng và cũng là điểm nhấn đáng chú ý ở Thiết bị mạng Linksys WHW0303, ta có công nghệ Seamless Roaming giúp chuyển kết nối tới những điểm phát sóng mạnh hơn, cho phép chuyển vùng không gián đoạn để trải nghiệm live stream hay đơn giản là xem video của bạn vẫn được giữ nguyên vẹn ngay cả khi đang di chuyển.</p>
+            </article>
             <h4 class="title_tskt uk-text-uppercase uk-margin">đặc điểm kỹ thuật</h4>
             <div class="uk-overflow-auto">
-
+                <table class="uk-table uk-table-striped">
+                    <tbody>
+                    <tr>
+                        <td>Tần số</td>
+                        <td>2.4GHz</td>
+                    </tr>
+                    <tr>
+                        <td>Màu sắc</td>
+                        <td>đen</td>
+                    </tr>
+                    <tr>
+                        <td>Thời hạn bảo hành</td>
+                        <td>12 ngày</td>
+                    </tr>
+                    <tr>
+                        <td>Tốc độ chuyển dữ liệu</td>
+                        <td>300Mbps</td>
+                    </tr>
+                    <tr>
+                        <td>Nguồn gốc</td>
+                        <td>Chính hãng</td>
+                    </tr>
+                    <tr>
+                        <td>Model</td>
+                        <td>Cisco RV110W</td>
+                    </tr>
+                    <tr>
+                        <td>Kích thước</td>
+                        <td>168.5 x 130 x 31.5 mm</td>
+                    </tr>
+                    <tr>
+                        <td>Thông số nổi bật</td>
+                        <td>Chuẩn kết nối IEE 802.11n, IEE 802.11g, IEE 802.11b, awngten 9dBi</td>
+                    </tr>
+                    <tr>
+                        <td>Màu sắc</td>
+                        <td>Xanh lá - Đen</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
             <h4 class="title_tskt uk-text-uppercase uk-margin">nhận xét</h4>
             <div class="uk-child-width-1-2@m uk-grid-medium" uk-grid>
